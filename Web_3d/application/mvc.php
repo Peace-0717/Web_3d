223d09a8598dfe29552936f1dfd12ba946a9cecb
Peace-0717 <119487670+Peace-0717@users.noreply.github.com>
2023-08-07 14:28:16 +0800

    Add files via upload

---
+++ Web_3d/application/mvc.php
@@ -0,0 +1,23 @@
+<?php
+
+require "view/load.php";
+require "model/model.php";
+require "controller/controller.php";
+
+$pageURI = $_SERVER["REQUEST_URI"];
+$pageURI = substr($pageURI, strrpos($pageURI, "index.php") + 10);
+
+if(strlen($pageURI) == 0) {
+	header("Location: /~yj226/3dapp/assignment/index.php/home");
+	new Controller("home");
+} elseif($pageURI == "home") {
+	new Controller("home");
+} elseif($pageURI == "drinks" || substr($pageURI, 0, 7) == "drinks?") {
+	new Controller("drinks");
+} elseif($pageURI == "builddb") {
+	new Controller("builddb");
+} else {
+	new Controller("notfound");
+}
+
+?>
